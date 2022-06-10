0de73e721ac62399bc910f8c663a42d0615b258c
thaianramalho <thaianramalho9@gmail.com>
2022-06-10 01:06:41 -0300

    verificacao redireciona

--- assets/js/dadosformulario.js
+++ assets/js/dadosformulario.js
@@ -1,29 +1,31 @@
-    const nome = document.getElementById("nome");
-    const vnome = nome.value;
+    var nome = document.getElementById("nome");
+    var vnome = nome.value;
 
-    const telefone = document.getElementById("telefone");
-    const vtelefone = telefone.value;
+    var telefone = document.getElementById("telefone");
+    var vtelefone = telefone.value;
 
-    const cep = document.getElementById("cep");
-    const vcep = cep.value;
+    var cep = document.getElementById("cep");
+    var vcep = cep.value;
 
-    const rua = document.getElementById("rua");
-    const vrua = rua.value;
+    var rua = document.getElementById("rua");
+    var vrua = rua.value;
 
-    const bairro = document.getElementById("bairro");
-    const vbairro = bairro.value;
+    var bairro = document.getElementById("bairro");
+    var vbairro = bairro.value;
 
-    const cidade = document.getElementById("cidade");
-    const vcidade = cidade.value;
+    var cidade = document.getElementById("cidade");
+    var vcidade = cidade.value;
 
-    const uf = document.getElementById("uf");
-    const vuf = uf.value;
+    var uf = document.getElementById("uf");
+    var vuf = uf.value;
 
     
     function redireciona(){
-        if (vnome != "" && vtelefone != "" && vtelefone != "" && vcep != "" && vrua != "" && vbairro != "" && vcidade != "" && vuf != "")
-        { site()
-        } else { error()
+        if (vnome != "" && vtelefone != "" && vcep != "" && vrua != "" && vbairro != "" && vcidade != "" && vuf != ""){
+            site()
+        }
+        else{
+            error()
         }};
 
     function site(){
