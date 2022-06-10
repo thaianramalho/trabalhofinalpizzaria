25fe54a4f5b092e2f90e8ae8647d3a5dd5958a9b
thaianramalho <thaianramalho9@gmail.com>
2022-06-10 15:39:28 -0300

    resolvido o maior bug desse site (pagamento)

--- assets/js/dadosformulario.js
+++ assets/js/dadosformulario.js
@@ -1,13 +1,22 @@
+const nome = document.getElementById("nome");
+
+const telefone = document.querySelector("#telefone");
+
+const cep = document.querySelector("#cep");
+
+const rua = document.querySelector("#rua");
+
+const bairro = document.querySelector("#bairro");
+
+const cidade = document.querySelector("#cidade");
+
+const uf = document.querySelector("#uf");
+
 function enviar(){
-    if (todas(nome) != ""){
-        window.open("pagamento.html","_self");
-    }};
-
-function site(){
-    window.open("pagamento.html","_self");
+    if ((nome.value != '')&&(telefone.value != '')&&(cep.value != '')){
+    window.open('pagamento.html', '_self');
+    // console.log(nome.value, telefone.value, cep.value, rua.value, bairro.value, cidade.value, uf.value);
 }
-
-
-function todas(nome){
-    document.getElementById('nome').value;
-}
+else{
+    alert('preencha todos os campos')
+}};
