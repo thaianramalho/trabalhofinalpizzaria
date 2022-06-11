d722f5c39fde85935371a053fec5e8e0e54beca2
thaianramalho <thaianramalho9@gmail.com>
2022-06-11 03:22:48 -0300

    trabalho finalizado (em codigo)

--- assets/js/dadosformulario.js
+++ assets/js/dadosformulario.js
@@ -1,17 +1,10 @@
 const nome = document.getElementById("nome");
-var nomev = nome.value;
 const telefone = document.getElementById("telefone");
-var telefonev = telefone.value;
 const cep = document.getElementById("cep");
-var cepv = cep.value;
 const rua = document.getElementById("rua");
-var ruav = rua.value;
 const bairro = document.getElementById("bairro");
-var bairrov = bairro.value;
 const cidade = document.getElementById("cidade");
-var cidadev = cidade.value;
 const uf = document.getElementById("uf");
-var ufv = uf.value;
 
 document.getElementById("nome1") == nomev;
 document.getElementById("telefone1") == telefonev;
